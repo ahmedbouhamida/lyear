9fe20655f9990ba1afed4c6dcd3cf1259e067b3c
unknown <ahmed2bouhamida@gmail.com>
2024-01-19 16:12:51 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+
+document.getElementById("Btn").addEventListener("click", myFunction);
+
+ function fun() {
+    const y = document.getElementById('year').value;
+    if( y % 4 === 0 ){
+        document.getElementById("out").innerHTML = `${y} is a leap year`;}
+        else if ( y % 100 === 0 && y % 400!= 0){
+            document.getElementById("out").innerHTML = `${y} is not a leap year`;
+        }
+      else {
+        document.getElementById("out").innerHTML = `${y} is not a leap year`;
+    }
+ }
+ 
