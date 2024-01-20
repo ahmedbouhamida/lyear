8c8f50d6c88ab594bba753867fe5482454f7ba47
unknown <ahmed2bouhamida@gmail.com>
2024-01-20 07:02:49 +0100

    fix main

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ document.getElementById("Btn").addEventListener("click", myFunction);
  function fun() {
     const y = document.getElementById('year').value;
     if(y==="") return;
-    else if( y % 4 === 0 ){
+    if( y % 4 === 0 ){
         document.getElementById("out").innerHTML = `${y} is a leap year`;}
-        if ( y % 100 === 0 && y % 400!= 0){
+        else if ( y % 100 === 0 && y % 400!= 0){
             document.getElementById("out").innerHTML = `${y} is not a leap year`;
         }
       else {
